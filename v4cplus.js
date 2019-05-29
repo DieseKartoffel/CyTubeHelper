12691cabada2e1555b9b8f8f91b1f4384af1e17d
Kart Offel <diesekartoffel@gmx.de>
2019-05-29 16:42:36 +0200

    Version 1.1: Track chat emotes in dialog window. When clicked paste identifier into chat box ready to send

--- v4cplus.js
+++ v4cplus.js
@@ -7,13 +7,18 @@
 *
 */
 
-$('#wrap').append('<div id="helper">Help</div>');
+$('#wrap').append(	'<div id="windowContent">'+
+						
+							'<div id="chatEmotes">'+
+							'</div>'+
+
+					'</div>'	);
 
 var minimized = true;
-var dialog = $("#helper").dialog({
-	width: 400,
-	height: 100,
+var dialog = $("#windowContent").dialog({
+
 	title: 'v4c Helper',
+	
 	beforeClose: function (event, ui) {
 		if (minimized == true) {
 			unminimize();
@@ -29,6 +34,12 @@ var dialog = $("#helper").dialog({
 	draggable: true,
 });
 
+
+//Fix JQuery Bug #8506, #9832 and #10069 (jQuery UI Dialog on resizing shrinks the content of the Dialog)
+$('.ui-dialog').css({'box-sizing': 'content-box'});
+$('.ui-dialog-content').css({'box-sizing': 'content-box'});
+
+//hide window
 function minimize() {
 	dialog.parents('.ui-dialog').animate({
 		height: '40px',
@@ -50,25 +61,56 @@ minimize();
 
 /*
 *
-*				TRACK EMOTES
+*				Make Chat emotes clickable and add them to Dialog:
 *
 */
 
-//select target
+// select target chat window
 var target = document.querySelector('#messagebuffer');
 
+// new message event handling
 var observer = new MutationObserver(function (mutations) {
 	var last = $('#messagebuffer').children().last();
 	//for each emote: onClick: append emote title attribute to chat input field
 	var emote = last.find(".channel-emote").on("click", function () {
-		//console.log($(this).attr("title") + " " + $(this).attr("src"));
 		$('#chatline').val( $('#chatline').val() + " " + ($(this).attr("title")) );		
-	});
-  console.log(emote.length + " emotes handled by v4cHelper")
+	});	
+
+	emote.each(function() {
+        console.log($(this).attr("title"), $(this).attr("src"));
+		emoteToDialog($(this).attr("title"), $(this).attr("src"));
+    });
+	
+    console.log(emote.length + " emotes handled by v4cHelper")
 });
-
-// Observe addition of new messages as div children
+// Observe addition of new messages that are div children of target window
 var config = { childList: true };
-
 // start observing
 observer.observe(target, config);
+
+
+
+// Add Emote to Dialog
+function emoteToDialog(title, src) {
+	if(typeof title == 'undefined' || typeof src == 'undefined'){
+		return;
+	}
+
+	var emoteID = title.replace("/","")
+	
+	//if emote is already listed in the UI: remove it
+	$('#'+ emoteID ).remove();
+	
+	//prepate html to prepend
+    var btn = $('<img src="'+src+'" id="'+ emoteID+'" width="28" height="28" /></img>');
+    btn.on("click", function () {
+        emoteButtonClick(title);
+    });
+
+	//add new emote button at top of list in UI
+	btn.prependTo("#chatEmotes");
+}
+
+function emoteButtonClick(title){
+	$('#chatline').val( $('#chatline').val() + " " + title );	
+}
